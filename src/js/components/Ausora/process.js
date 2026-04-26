69fad08cf0a9c19e9938edd27c285da4f26a89b6
Daniil <daniil@notdiy.studio>
2026-04-26 17:01:08 +0200

    process v2

--- src/js/components/Ausora/process.js
+++ src/js/components/Ausora/process.js
@@ -1,39 +1,54 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const greyColor = "#9fa5a5";
 
-const processFadeIn = (process, index, diagram) => {
+const processFadeIn = (index, diagram) => {
   const tl = gsap.timeline();
   const captionsBlocks = diagram.querySelectorAll(
+    ".au-process_diagram_caption-wrapper",
+  );
+  const heading = captionsBlocks[index].querySelector(
+    ".au-process_diagram_caption-heading",
+  );
+  const caption = captionsBlocks[index].querySelector(
     ".au-process_diagram_caption-paragraph",
   );
-  let captions = [];
-  if (index === 3) {
-    captions.push(captionsBlocks[3]);
-    captions.push(captionsBlocks[4]);
+  const processes = diagram.querySelectorAll(".au-process_diagram_process");
+  let process;
+  if (index === 4) {
+    process = processes[3];
   } else {
-    captions.push(captionsBlocks[index]);
+    process = processes[index];
   }
 
-  tl.to(process, { color: "black" });
-  tl.to(captions, { opacity: 1 }, "<");
+  tl.to(process, { color: "black", duration: 0.2, ease: "power2.out" });
+  tl.to(caption, { opacity: 1, duration: 0.2, ease: "power2.out" }, "<");
+  tl.to(heading, { color: "black", duration: 0.2, ease: "power2.out" }, "<");
 };
 
-const processFadeOut = (process, index, diagram) => {
+const processFadeOut = (index, diagram) => {
   const tl = gsap.timeline();
   const captionsBlocks = diagram.querySelectorAll(
+    ".au-process_diagram_caption-wrapper",
+  );
+  const heading = captionsBlocks[index].querySelector(
+    ".au-process_diagram_caption-heading",
+  );
+  const caption = captionsBlocks[index].querySelector(
     ".au-process_diagram_caption-paragraph",
   );
-  let captions = [];
-  if (index === 3) {
-    captions.push(captionsBlocks[3]);
-    captions.push(captionsBlocks[4]);
+  const processes = diagram.querySelectorAll(".au-process_diagram_process");
+  let process;
+  if (index === 4) {
+    process = processes[3];
   } else {
-    captions.push(captionsBlocks[index]);
+    process = processes[index];
   }
 
-  tl.to(process, { color: greyColor });
-  tl.to(captions, { opacity: 0 }, "<");
+  tl.to(process, { color: greyColor, duration: 0.2, ease: "power2.out" });
+  tl.to(caption, { opacity: 0, duration: 0.2, ease: "power2.out" }, "<");
+  tl.to(heading, { color: greyColor, duration: 0.2, ease: "power2.out" }, "<");
 };
 
 const animationDesktop = (page) => {
@@ -41,16 +56,51 @@ const animationDesktop = (page) => {
   if (!diagrams.length) return;
 
   diagrams.forEach((diagram) => {
-    const processes = diagram.querySelectorAll(".au-process_diagram_process");
-    if (!processes.length) return;
+    const triggers = diagram.querySelectorAll(
+      ".au-process_diagram_process-trigger",
+    );
+    if (!triggers.length) return;
 
-    processes.forEach((process, index) => {
-      processFadeOut(process, index, diagram);
-      process.addEventListener("mouseenter", () => {
-        processFadeIn(process, index, diagram);
+    triggers.forEach((trigger, index) => {
+      processFadeOut(index, diagram);
+
+      trigger.addEventListener("mouseenter", () => {
+        processFadeIn(index, diagram);
       });
-      process.addEventListener("mouseleave", () => {
-        processFadeOut(process, index, diagram);
+      trigger.addEventListener("mouseleave", () => {
+        processFadeOut(index, diagram);
+      });
+    });
+  });
+};
+
+const animationMobile = (page) => {
+  const diagrams = page.querySelectorAll(".au-process_diagram");
+  if (!diagrams.length) return;
+
+  diagrams.forEach((diagram) => {
+    const triggers = diagram.querySelectorAll(
+      ".au-process_diagram_process-trigger",
+    );
+    const scroller = diagram.parentNode.parentNode;
+    const captionsBlocks = diagram.querySelectorAll(
+      ".au-process_diagram_caption-wrapper",
+    );
+    if (!triggers.length) return;
+
+    triggers.forEach((trigger, index) => {
+      processFadeOut(index, diagram);
+
+      ScrollTrigger.create({
+        trigger: captionsBlocks[index],
+        scroller: scroller,
+        horizontal: true,
+        start: "left 80%",
+        end: "right 10%",
+        onEnter: () => processFadeIn(index, diagram),
+        onEnterBack: () => processFadeIn(index, diagram),
+        onLeave: () => processFadeOut(index, diagram),
+        onLeaveBack: () => processFadeOut(index, diagram),
       });
     });
   });
@@ -59,6 +109,8 @@ const animationDesktop = (page) => {
 const processInit = (page) => {
   if (window.innerWidth > 991) {
     animationDesktop(page);
+  } else {
+    animationMobile(page);
   }
 };
 
